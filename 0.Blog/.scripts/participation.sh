7a71ff261fe287864316687420c0305f51c888c0
setrar <setrar@valiha.com>
2022-09-13 18:40:47 -0400

    .

--- 0.Blog/.scripts/participation.sh
+++ 0.Blog/.scripts/participation.sh
@@ -37,7 +37,7 @@ i=0
 
 for id in "${ETUDIANTS[@]}"
 do
-   FILE=${id}/README.md
+   FILE=../${id}/README.md
    OK="| ${i} | [${id}](./${FILE}) - <image src='https://avatars0.githubusercontent.com/u/${AVATARS[$i]}?s=460&v=4' width=20 height=20></image> | :heavy_check_mark: | "
    KO="| ${i} | [${id}](./${FILE}) - <image src='https://avatars0.githubusercontent.com/u/${AVATARS[$i]}?s=460&v=4' width=20 height=20></image> | :x: | "
    if [ -f "$FILE" ]; then
